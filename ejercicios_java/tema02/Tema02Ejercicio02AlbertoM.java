8bd43044e1a1beb588e0b3d5c6801475a94ead9d
Alberto Moreno González <albertomorenogonzalez03@gmail.com>
2021-10-14 10:44:54 +0200

    Añadido el ejercicio 2 del Tema 2

---
+++ ejercicios_java/tema02/Tema02Ejercicio02AlbertoM.java
@@ -0,0 +1,14 @@
+/**
+ * Crea la variable nombre y asígnale tu nombre completo. Muestra su valor por
+ * pantalla de tal forma que el resultado del programa sea el mismo que en el
+ * ejercicio 1 del capítulo 1.
+ *
+ * @author Alberto Moreno González
+ */
+public class Tema02Ejercicio02AlbertoM {
+  public static void main(String[] args) {
+    String nombre = "Alberto Moreno González";
+    
+    System.out.println(nombre);
+  }
+}
