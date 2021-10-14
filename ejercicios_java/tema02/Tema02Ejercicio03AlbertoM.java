63c9877219d48b881889e4dbd45c878e7a0defb9
Alberto Moreno González <albertomorenogonzalez03@gmail.com>
2021-10-14 11:00:04 +0200

    Añadido el ejercicio 3 del Tema 2

---
+++ ejercicios_java/tema02/Tema02Ejercicio03AlbertoM.java
@@ -0,0 +1,17 @@
+/**
+ * Crea las variables nombre, direccion y telefono y asígnales los valores correspondientes. Muestra los valores de esas variables por pantalla de tal forma que el
+ * resultado del programa sea el mismo que en el ejercicio 2.
+ *
+ * @author Alberto Moreno González
+ */
+public class Tema02Ejercicio03AlbertoM {
+  public static void main(String[] args) {
+    String nombre = "Alberto Moreno González";
+    String direccion = "C/ Manuel de Falla nº 21, Pizarra, Málaga";
+    String telefono = "655 63 16 07";
+    
+    System.out.println(nombre);
+    System.out.println("Dirección: " + direccion);
+    System.out.println("Nº Teléfono. " + telefono);
+  }
+}
