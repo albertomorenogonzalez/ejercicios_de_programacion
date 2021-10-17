4457e2e80e31e5e900592230be35736a186bb752
Alberto Moreno González <albertomorenogonzalez03@gmail.com>
2021-10-17 14:14:13 +0200

    Añadido ejercicio 7 del Tema 2 (completo).

--- ejercicios_java/tema02/Tema02Ejercicio07AlbertoM.java
+++ ejercicios_java/tema02/Tema02Ejercicio07AlbertoM.java
@@ -2,14 +2,23 @@
  * Escribe un programa que declare variables de tipo char y de tipo String. Intenta
  * mostrarlas por pantalla todas juntas en la misma línea y con una sola sentencia
  * de Java (con un solo println) ¿es posible?
+ * 
+ * Si, es posible, tal y como se muestra a continuación.
  *
  * @author Alberto Moreno González
  */
-public class Tema02Ejercicio06AlbertoM {
+public class Tema02Ejercicio07AlbertoM {
   public static void main(String[] args) {
     String texto1 = "Las tres primeras letras ";
     String texto2 = "del abecedario ";
     String texto3 = "son: ";
+    char letra1 = 'a';
+    String texto4 = ", ";
+    char letra2 = 'b';
+    String texto5 = " y ";
+    char letra3 = 'c';
+    String dot = ".";
     
+    System.out.println(texto1 + texto2 + texto3 + letra1 + texto4 + letra2 + texto5 + letra3 + dot);
   }
 }
