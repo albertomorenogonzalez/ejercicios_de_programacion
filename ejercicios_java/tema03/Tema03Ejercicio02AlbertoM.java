f624bf17029b0c7c88758e3ed579f5f5c3d098ad
Alberto Moreno González <albertomorenogonzalez03@gmail.com>
2021-10-18 21:03:12 +0200

    Añadido el ejercicio 2 del tema 3 (completo, incompleto en la subida anterior

---
+++ ejercicios_java/tema03/Tema03Ejercicio02AlbertoM.java
@@ -0,0 +1,25 @@
+/**
+ * Realiza un conversor de euros a pesetas. La cantidad de euros que se quiere
+ * convertir debe ser introducida por teclado.
+ *
+ * @author Alberto Moreno González
+ */
+public class Tema03Ejercicio02AlbertoM {
+  public static void main(String[] args) {
+    
+    String number;
+    
+    System.out.println("Por favor, introduzca la cantidad en euros que quiere convertir: ");
+    System.out.print("> ");
+    number = System.console().readLine();
+    int euros;
+    euros = Integer.parseInt( number );
+    
+    System.out.println(" ");
+    
+    System.out.print(euros);
+    System.out.print(" € son ");
+    System.out.print(euros * 166.386);
+    System.out.println(" pesetas.");
+  }
+}
