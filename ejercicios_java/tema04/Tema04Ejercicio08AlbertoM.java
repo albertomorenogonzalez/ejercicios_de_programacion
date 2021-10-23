3c4a2bf570353f141fc9dd4b3a884de5ea54749d
Alberto Moreno González <albertomorenogonzalez03@gmail.com>
2021-10-23 15:16:18 +0200

    Añadido ejercicio 8 tema 4. Error solucionado en ejercicio 7 tema 4

--- ejercicios_java/tema04/Tema04Ejercicio08AlbertoM.java
+++ ejercicios_java/tema04/Tema04Ejercicio08AlbertoM.java
@@ -1,9 +1,10 @@
 /**
- * Realiza un programa que calcule la media de tres notas.
+ * Amplía el programa anterior para que diga la nota del boletín (insuficiente,
+ * suficiente, bien, notable o sobresaliente).
  *
  * @author Alberto Moreno González
  */
-public class Tema04Ejercicio07AlbertoM {
+public class Tema04Ejercicio08AlbertoM {
   public static void main(String[] args) {
     System.out.println("PROGRAMA QUE CALCULA LA MEDIA DE TRES NOTAS");
     System.out.println("--------------------------------------------");
@@ -32,13 +33,22 @@ public class Tema04Ejercicio07AlbertoM {
       if ((media>=0) && (media<5)) {
       System.out.print("El alumno ha sacado una media de ");
       System.out.printf("%.2f" ,media);
-      System.out.println(", por lo tanto, ha suspendido.");
+      System.out.println(", un insuficiente, por lo tanto, ha suspendido.");
       } else if ((media>=5) && (media<=10)) {
         System.out.print("El alumno ha sacado una media de ");
         System.out.printf("%.2f" ,media);
         System.out.print(".");
+        if ((media>=5) && (media<6)) {
+          System.out.println(" Un suficente, ha aprobado.");
+        }
+        if ((media>=6) && (media<7)) {
+          System.out.println(" Un bien.");
+        }
+        if ((media>=7) && (media<9)) {
+          System.out.println(" Un notable, ¡bien!");
+        }
         if ((media>=9) && (media<=10)) {
-          System.out.println(" ¡Enhorabuena!");
+          System.out.println(" Un sobresaliente, ¡enhorabuena!");
         }
       } else {
         System.out.println("Ha habido un error, reinicie el programa.");
