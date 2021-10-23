e229446071ebf067d16d23e17b16213810d45422
Alberto Moreno González <albertomorenogonzalez03@gmail.com>
2021-10-23 13:43:47 +0200

    Añadido Ejericio 4 tema 4

---
+++ ejercicios_java/tema04/Tema04Ejercicio04AlbertoM.java
@@ -0,0 +1,33 @@
+/**
+ * Vamos a ampliar uno de los ejercicios de la relación anterior para considerar
+ * las horas extras. Escribe un programa que calcule el salario semanal de un
+ * trabajador teniendo en cuenta que las horas ordinarias (40 primeras horas de
+ * trabajo) se pagan a 12 euros la hora. A partir de la hora 41, se pagan a 16 euros
+ * la hora.
+ *
+ * @author Alberto Moreno González
+ */
+public class Tema04Ejercicio04AlbertoM {
+  public static void main(String[] args) {
+    System.out.println("PROGRAMA QUE CALCULA EL SALARIO SEMANAL DE UNA PERSONA TENIENDO EN CUENTA LAS HORAS TRABAJADAS");
+    System.out.println("-----------------------------------------------------------------------------------------------");
+    
+    System.out.println("Por favor, introduzca cuantas horas ha trabajado el empleado esta semana: ");
+    System.out.print("> ");
+    int horasdetrabajo = Integer.parseInt(System.console().readLine());
+    
+    System.out.println(" ");
+    
+    if ((horasdetrabajo>0) && (horasdetrabajo<=40)) {
+      System.out.print("El empleado debe cobrar ");
+      System.out.print(horasdetrabajo * 12);
+      System.out.println(" € esta semana.");
+    } else if (horasdetrabajo>40) {
+        System.out.print("El empleado debe cobrar ");
+        System.out.print((40 * 12) + (horasdetrabajo - 40) * 16);
+        System.out.println(" € esta semana.");
+    } else {
+        System.out.println("El trabajador no ha podido trabajar este número de horas.");
+    }
+  }
+}
