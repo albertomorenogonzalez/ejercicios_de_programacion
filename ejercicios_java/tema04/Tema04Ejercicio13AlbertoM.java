8f2979bd1896f4c7dae9446dfb9ca4ab829de800
Alberto Moreno González <albertomorenogonzalez03@gmail.com>
2021-10-24 23:52:24 +0200

    Solucionados problemas en ejercicio 13 tema 4

--- ejercicios_java/tema04/Tema04Ejercicio13AlbertoM.java
+++ ejercicios_java/tema04/Tema04Ejercicio13AlbertoM.java
@@ -6,71 +6,97 @@
 public class Tema04Ejercicio13AlbertoM {
   public static void main(String[] args) {
     System.out.println("PROGRAMA QUE ORDENA TRES NÚMEROS INTRODUCIDOS POR TECLADO");
-    System.out.println("--------------------------------------------------------------");
+    System.out.println("----------------------------------------------------------");
     
     System.out.println("Introduzca un número, por favor: ");
     System.out.print("> ");
-    double n1 = Double.parseDouble(System.console().readLine());
+    int n1 = Integer.parseInt(System.console().readLine());
     System.out.println(" ");
     
     System.out.println("Introduzca otro número, por favor: ");
     System.out.print("> ");
-    double n2 = Double.parseDouble(System.console().readLine());
+    int n2 = Integer.parseInt(System.console().readLine());
     System.out.println(" ");
     
     System.out.println("Introduzca otro número, por favor: ");
     System.out.print("> ");
-    double n3 = Double.parseDouble(System.console().readLine());
+    int n3 = Integer.parseInt(System.console().readLine());
     System.out.println(" ");
     
     if ((n1 > n2) && (n1 > n3) && (n2 > n3)) {
-      System.out.printf("%.2f" ,n1);
+      System.out.print(n1);
       System.out.print(" > ");
-      System.out.printf("%.2f" ,n2);
+      System.out.print(n2);
       System.out.print(" > ");
-      System.out.printf("%.2f" ,n3);
-      System.out.print(".");
+      System.out.print(n3);
     } else if ((n1 > n2) && (n1 > n3) && (n3 > n2)) {
-        System.out.printf("%.2f" ,n1);
+        System.out.print(n1);
         System.out.print(" > ");
-        System.out.printf("%.2f" ,n3);
+        System.out.print(n3);
         System.out.print(" > ");
-        System.out.printf("%.2f" ,n2);
-        System.out.print(".");
+        System.out.print(n2);
     } else {
         if ((n2 > n1) && (n2 > n3) && (n1 > n3)) {
-          System.out.printf("%.2f" ,n2);
+          System.out.print(n2);
           System.out.print(" > ");
-          System.out.printf("%.2f" ,n1);
+          System.out.print(n1);
           System.out.print(" > ");
-          System.out.printf("%.2f" ,n3);
-          System.out.print(".");
+          System.out.print(n3);
         } else if ((n2 > n1) && (n2 > n3) && (n3 > n1)) {
-            System.out.printf("%.2f" ,n2);
+            System.out.print(n2);
             System.out.print(" > ");
-            System.out.printf("%.2f" ,n3);
+            System.out.print(n3);
             System.out.print(" > ");
-            System.out.printf("%.2f" ,n1);
-            System.out.print(".");
+            System.out.print(n1);
         } else {
             if ((n3 > n1) && (n3 > n2) && (n1 > n2)) {
-              System.out.printf("%.2f" ,n3);
+              System.out.print(n3);
               System.out.print(" > ");
-              System.out.printf("%.2f" ,n1);
+              System.out.print(n1);
               System.out.print(" > ");
-              System.out.printf("%.2f" ,n2);
-              System.out.print(".");
+              System.out.print(n2);
             } else if ((n3 > n1) && (n3 > n2) && (n2 > n1)) {
-                System.out.printf("%.2f" ,n3);
+                System.out.print(n3);
                 System.out.print(" > ");
-                System.out.printf("%.2f" ,n1);
+                System.out.print(n2);
                 System.out.print(" > ");
-                System.out.printf("%.2f" ,n2);
-                System.out.print(".");
+                System.out.print(n1);
             } else {
-                System.out.print("Los tres números son iguales.");
+                if ((n1 == n2) && (n1 > n3)) {
+                  System.out.print(n1);
+                  System.out.print(" > ");
+                  System.out.print(n3);
+                } else if ((n1 == n2) && (n3 > n1)) {
+                    System.out.print(n3);
+                    System.out.print(" > ");
+                    System.out.print(n1);
+                } else if ((n1 == n3) && (n2 > n1)) {
+                    System.out.print(n2);
+                    System.out.print(" > ");
+                    System.out.print(n1);
+                } else if ((n1 == n3) && (n1 > n2)) {
+                    System.out.print(n1);
+                    System.out.print(" > ");
+                    System.out.print(n2);
+                } else if ((n2 == n3) && (n1 > n2)) {
+                    System.out.print(n1);
+                    System.out.print(" > ");
+                    System.out.print(n2);
+                } else if ((n2 == n3) && (n2 > n1)) {
+                    System.out.print(n2);
+                    System.out.print(" > ");
+                    System.out.print(n1);
+                } else {
+                    System.out.print("Los tres números son iguales.");
+                }
             }
-          }
         }
+    }
   }
 }
+
+
+
+
+
+
