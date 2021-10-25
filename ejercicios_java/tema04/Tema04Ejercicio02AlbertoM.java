d1e88becacf322d6c107efeb763de6e5d79a5936
Alberto Moreno González <albertomorenogonzalez03@gmail.com>
2021-10-25 08:58:39 +0200

    Corrección en ejercicio 2 tema 4

--- ejercicios_java/tema04/Tema04Ejercicio02AlbertoM.java
+++ ejercicios_java/tema04/Tema04Ejercicio02AlbertoM.java
@@ -19,16 +19,14 @@ public class Tema04Ejercicio02AlbertoM {
     
     if ((hora>=0) && (hora<=5)) {
       System.out.println("¡Buenas noches!");
-    } if ((hora >= 6) && (hora <= 12)) {
+    } else if ((hora >= 6) && (hora <= 12)) {
         System.out.println("¡Buenos días!");
-    } if ((hora >= 13) && (hora <= 20)) {
+    } else if ((hora >= 13) && (hora <= 20)) {
         System.out.println("¡Buenas tardes!");
-    } if ((hora >= 21) && (hora <= 23)) {
+    } else if ((hora >= 21) && (hora <= 23)) {
         System.out.println("¡Buenas noches!");
     } else {
-        if ((hora<0) || (hora>23)) {
-          System.out.println("La hora introducida no existe.");
-      }
+        System.out.println("La hora introducida no existe.");
     }
   }
 }
