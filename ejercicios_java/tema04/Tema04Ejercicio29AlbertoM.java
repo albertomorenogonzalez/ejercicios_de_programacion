85f1f6c05d70106876e9ef8050a57a41c3c9399a
Alberto Moreno González <albertomorenogonzalez03@gmail.com>
2021-11-03 13:33:18 +0100

    Corrección ejercicios tema 4

--- ejercicios_java/tema04/Tema04Ejercicio29AlbertoM.java
+++ ejercicios_java/tema04/Tema04Ejercicio29AlbertoM.java
@@ -19,42 +19,58 @@ public class Tema04Ejercicio29AlbertoM {
     String comida = (System.console().readLine().toLowerCase());
     System.out.println(" ");
     
+    double precioComida = 0;
+    String bebida;
+    double precioBebida = 0;
+    
     switch (comida) {
       case "palmera":
+        precioComida = 1.40;
+      
         System.out.println("¿Qué ha tomado de beber? (zumo o café):");
         System.out.print("> ");
-        String bebida1 = (System.console().readLine().toLowerCase());
+        bebida = (System.console().readLine().toLowerCase());
         System.out.println(" ");
         
-        if ("zumo".equals(bebida1)) {
-            System.out.printf("%-20s            %.2f %-1s\n","Palmera:" ,1.40 ,"€");
-            System.out.printf("%-20s            %.2f %-1s\n","Zumo:" ,1.50 ,"€");
-            System.out.printf("%-20s            %.2f %-1s\n","Total:" ,(1.40 + 1.50) ,"€");
-        } else if ("café".equals(bebida1)) {
-            System.out.printf("%-20s            %.2f %-1s\n","Palmera:" ,1.40 ,"€");
-            System.out.printf("%-20s            %.2f %-1s\n","Café:" ,1.20 ,"€");
-            System.out.printf("%-20s            %.2f %-1s\n","Total:" ,(1.40 + 1.20) ,"€");
+        System.out.printf("%-20s            %.2f %-1s\n","Palmera:" ,precioComida ,"€");
+        
+        if ("zumo".equals(bebida)) {
+            precioBebida = 1.50;
+            
+            System.out.printf("%-20s            %.2f %-1s\n","Zumo:" ,precioBebida ,"€");
+        } else if ("café".equals(bebida)) {
+             precioBebida = 1.20;
+            
+            System.out.printf("%-20s            %.2f %-1s\n","Café:" ,precioBebida,"€");
         } else {
             System.out.println("Introduzca correctamente los datos.");
         }
+      
+        System.out.printf("%-20s            %.2f %-1s\n","Total:" ,(precioComida + precioBebida) ,"€");
       break;
       case "donut":
+        precioComida = 1.00;
+      
         System.out.println("¿Qué ha tomado de beber? (zumo o café):");
         System.out.print("> ");
-        String bebida2 = (System.console().readLine().toLowerCase());
+        bebida = (System.console().readLine().toLowerCase());
         System.out.println(" ");
         
-        if ("zumo".equals(bebida2)) {
-            System.out.printf("%-20s            %.2f %-1s\n","Donut:" ,1.00 ,"€");
-            System.out.printf("%-20s            %.2f %-1s\n","Zumo:" ,1.50 ,"€");
-            System.out.printf("%-20s            %.2f %-1s\n","Total:" ,(1.00 + 1.50) ,"€");
-        } else if ("café".equals(bebida2)) {
-            System.out.printf("%-20s            %.2f %-1s\n","Donut:" ,1.00 ,"€");
-            System.out.printf("%-20s            %.2f %-1s\n","Café:" ,1.20 ,"€");
-            System.out.printf("%-20s            %.2f %-1s\n","Total:" ,(1.00 + 1.20) ,"€");
+        System.out.printf("%-20s            %.2f %-1s\n","Donut:" ,precioComida ,"€");
+        
+        if ("zumo".equals(bebida)) {
+            precioBebida = 1.50;
+            
+            System.out.printf("%-20s            %.2f %-1s\n","Zumo:" ,precioBebida ,"€");
+        } else if ("café".equals(bebida)) {
+             precioBebida = 1.20;
+            
+            System.out.printf("%-20s            %.2f %-1s\n","Café:" ,precioBebida,"€");
         } else {
             System.out.println("Introduzca correctamente los datos.");
         }
+      
+        System.out.printf("%-20s            %.2f %-1s\n","Total:" ,(precioComida + precioBebida) ,"€");
       break;
       case "pitufo":
         System.out.println("¿De qué era el pitufo? (con aceite o de tortilla): ");
@@ -63,42 +79,52 @@ public class Tema04Ejercicio29AlbertoM {
         System.out.println(" ");
         
         if ("con aceite".equals(tipoPitufo)) {
+          precioComida = 1.20;
+      
           System.out.println("¿Qué ha tomado de beber? (zumo o café):");
           System.out.print("> ");
-          String bebida3 = (System.console().readLine().toLowerCase());
+          bebida = (System.console().readLine().toLowerCase());
           System.out.println(" ");
         
-          if ("zumo".equals(bebida3)) {
-            System.out.printf("%-20s            %.2f %-1s\n","Pitufo con aceite:" ,1.20 ,"€");
-            System.out.printf("%-20s            %.2f %-1s\n","Zumo:" ,1.50 ,"€");
-            System.out.printf("%-20s            %.2f %-1s\n","Total:" ,(1.20 + 1.50) ,"€");
-          } else if ("café".equals(bebida3)) {
-              System.out.printf("%-20s            %.2f %-1s\n","Pitufo con aceite:" ,1.00 ,"€");
-              System.out.printf("%-20s            %.2f %-1s\n","Café:" ,1.20 ,"€");
-              System.out.printf("%-20s            %.2f %-1s\n","Total:" ,(1.20 + 1.20) ,"€");
+          System.out.printf("%-20s            %.2f %-1s\n","Pitufo con aceite:" ,precioComida ,"€");
+        
+          if ("zumo".equals(bebida)) {
+            precioBebida = 1.50;
+            
+            System.out.printf("%-20s            %.2f %-1s\n","Zumo:" ,precioBebida ,"€");
+          } else if ("café".equals(bebida)) {
+              precioBebida = 1.20;
+            
+              System.out.printf("%-20s            %.2f %-1s\n","Café:" ,precioBebida,"€");
           } else {
               System.out.println("Introduzca correctamente los datos.");
           }
+          
+          System.out.printf("%-20s            %.2f %-1s\n","Total:" ,(precioComida + precioBebida) ,"€");
         } else if ("de tortilla".equals(tipoPitufo)) {
+            precioComida = 1.60;
+      
             System.out.println("¿Qué ha tomado de beber? (zumo o café):");
             System.out.print("> ");
-            String bebida4 = (System.console().readLine().toLowerCase());
+            bebida = (System.console().readLine().toLowerCase());
             System.out.println(" ");
         
-            if ("zumo".equals(bebida4)) {
-              System.out.printf("%-20s            %.2f %-1s\n","Pitufo de tortilla:" ,1.60 ,"€");
-              System.out.printf("%-20s            %.2f %-1s\n","Zumo:" ,1.50 ,"€");
-              System.out.printf("%-20s            %.2f %-1s\n","Total:" ,(1.60 + 1.50) ,"€");
-            } else if ("café".equals(bebida4)) {
-                System.out.printf("%-20s            %.2f %-1s\n","Pitufo de tortilla:" ,1.60 ,"€");
-                System.out.printf("%-20s            %.2f %-1s\n","Café:" ,1.20 ,"€");
-                System.out.printf("%-20s            %.2f %-1s\n","Total:" ,(1.60 + 1.20) ,"€");
+            System.out.printf("%-20s            %.2f %-1s\n","Pitufo de tortilla:" ,precioComida ,"€");
+        
+            if ("zumo".equals(bebida)) {
+              precioBebida = 1.50;
+            
+              System.out.printf("%-20s            %.2f %-1s\n","Zumo:" ,precioBebida ,"€");
+            } else if ("café".equals(bebida)) {
+                precioBebida = 1.20;
+            
+                System.out.printf("%-20s            %.2f %-1s\n","Café:" ,precioBebida,"€");
             } else {
                 System.out.println("Introduzca correctamente los datos.");
             }
-        } else { 
-            System.out.println("Introduzca correctamente los datos.");
-        }
+            
+          System.out.printf("%-20s            %.2f %-1s\n","Total:" ,(precioComida + precioBebida) ,"€");
+          }
         break;
       default: 
         System.out.println("Introduzca correctamente los datos.");
