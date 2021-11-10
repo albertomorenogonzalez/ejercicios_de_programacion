4953d7ca17ca04301a947895eb4b361091a250ac
Alberto Moreno González <albertomorenogonzalez03@gmail.com>
2021-11-10 19:25:24 +0100

    Añadido ejercico 18 tema 5 (completo)

--- ejercicios_java/tema05/Tema05Ejercicio18AlbertoM.java
+++ ejercicios_java/tema05/Tema05Ejercicio18AlbertoM.java
@@ -12,18 +12,13 @@ public class Tema05Ejercicio18AlbertoM {
     System.out.println("---------------------------------------------------------------------------------------------------------");
     System.out.println(" ");
     
+    System.out.println("Por favor, introduzca un número entero: ");
+    
     int numero1 = 0;
     
     int numero2 = 0;
     
-    System.out.println("Por favor, introduzca un número entero: ");
-    
     while (numero1 == numero2) {
-      if (numero1 != numero2) {
-        System.out.println("Por favor, introduzca un número entero: ");
-      } else {
-          System.out.println("Por favor, introduzca otro número entero distinto: ");
-      }
       System.out.print("> ");
       numero1 = Integer.parseInt(System.console().readLine());
       System.out.println(" ");
@@ -33,6 +28,7 @@ public class Tema05Ejercicio18AlbertoM {
       numero2 = Integer.parseInt(System.console().readLine());
       System.out.println(" ");
       
+    
       if (numero1 > numero2) {
         while (numero2 < numero1) {
           System.out.print(numero2 + " ");
@@ -49,6 +45,8 @@ public class Tema05Ejercicio18AlbertoM {
           }
         
         System.out.print(numero2);
+      } else { 
+          System.out.println("Por favor, introduzca dos números enteros distintos, primero uno: ");
       }
     }
   }
