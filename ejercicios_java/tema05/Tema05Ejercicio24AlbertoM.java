325ee98144fcc339e1220423fc2a30695fa3e099
Alberto Moreno González <albertomorenogonzalez03@gmail.com>
2021-11-14 19:33:50 +0100

    Añadido ejercico 24 tema 5 (completo)

--- ejercicios_java/tema05/Tema05Ejercicio24AlbertoM.java
+++ ejercicios_java/tema05/Tema05Ejercicio24AlbertoM.java
@@ -7,16 +7,17 @@
 public class Tema05Ejercicio24AlbertoM {
   public static void main(String[] args) {
     System.out.println("PROGRAMA QUE PINTA UNA PIRÁMIDE DE NUMEROS POR PANTALLA");
-    System.out.println("---------------------------------------------");
+    System.out.println("--------------------------------------------------------");
     System.out.println(" ");
     
     System.out.println("Introduzca la altura de la pirámide, por favor: ");
     System.out.print("> ");
     int altura = Integer.parseInt(System.console().readLine());
     System.out.println(" ");
+    System.out.println(" ");
     
     int linea = 1;
-    int numNumeros = 1;
+    int numNumeros = 0;
     int espacios = altura - 1;
     
     while (linea <= altura) {
@@ -26,21 +27,17 @@ public class Tema05Ejercicio24AlbertoM {
       }
       
       for (int longLinea = 1; longLinea <= numNumeros; longLinea++) {
-        int numeroDeMenos = 2;
-        
-        if (longLinea > linea) {
-            System.out.print(longLinea - numeroDeMenos);
-            
-            numeroDeMenos++;
-        } else {
-            System.out.print(longLinea);
-        }
+        System.out.print(longLinea); 
       }
       
-      System.out.println(" ");
+      for (int numeroLinea = linea; numeroLinea > 0; numeroLinea--) {
+        System.out.print(numeroLinea); 
+      }
+      
+      System.out.println(" "); 
       
       linea++;
-      numNumeros += 2;
+      numNumeros++;
       espacios--;
     }
   }
