f9962d2f392905744753c410fbdd2dbd7353d0d2
Alberto Moreno González <albertomorenogonzalez03@gmail.com>
2021-11-20 19:54:45 +0100

    Añadido ejercicio 40 tema 5

---
+++ ejercicios_java/tema05/Tema05Ejercicio40AlbertoM.java
@@ -0,0 +1,81 @@
+/**
+ * Realiza un programa que pinte por pantalla un rombo hueco hecho con
+ * asteriscos. El programa debe pedir la altura. Se debe comprobar que la altura
+ * sea un número impar mayor o igual a 3, en caso contrario se debe mostrar un
+ * mensaje de error.
+ *
+ * @author Alberto Moreno González
+ */
+public class Tema05Ejercicio40AlbertoM {
+  public static void main(String[] args) {
+    System.out.println("PROGRAMA QUE PINTA ROMBOS");
+    System.out.println("--------------------------");
+    System.out.println(" ");
+    
+    System.out.println("Introduzca la altura de la x, por favor (número impar): ");
+    
+    int altura = 4;
+    
+    while (((altura % 2) == 0) && (altura >= 3)) {
+      System.out.print("> ");
+      altura = Integer.parseInt(System.console().readLine());
+      System.out.println(" ");
+      System.out.println(" ");
+      
+      if ((altura % 2) == 0) {
+        System.out.println("Introduzca correctamente la altura de la x, por favor (número impar): ");
+      }
+    }
+    
+    int linea = (altura - (altura / 2));
+    int numCaracteres = 1;
+    int espacios = (altura / 2);
+    
+    while (linea <= altura) {
+      
+      for (int numEspacios = 1; numEspacios <= espacios; numEspacios++) {
+        System.out.print(" "); 
+      }
+      
+      for (int longLinea = 1; longLinea <= numCaracteres; longLinea++) {
+        if ((longLinea == 1) || (longLinea == numCaracteres)) {
+          System.out.print("*");
+        } else {
+          System.out.print(" ");
+        }
+      }
+      
+      System.out.println(" ");
+      
+      linea++;
+      numCaracteres += 2;
+      espacios--;
+    }
+    
+    
+    linea = (altura / 2);
+    numCaracteres = (linea * 2);
+    espacios = 1;
+    
+    while (linea >= 0) {
+      
+      for (int numEspacios = espacios; numEspacios >= 1; numEspacios--) {
+        System.out.print(" "); 
+      }
+      
+      for (int longLinea = numCaracteres; longLinea > 1; longLinea--) {
+        if ((longLinea == 2) || (longLinea == numCaracteres)) {
+          System.out.print("*");
+        } else {
+          System.out.print(" ");
+        }
+      }
+      
+      System.out.println(" ");
+      
+      linea--;
+      numCaracteres -= 2;
+      espacios++;
+    }
+  }
+}
