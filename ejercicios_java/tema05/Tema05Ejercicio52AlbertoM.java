75f0b9bb72caf5ea8ec492e983c1b28595459b40
Alberto <albertomorenogonzalez03@gmail.com>
2021-11-26 22:18:48 +0100

    Añadido ejercicio 52 tema 5

---
+++ ejercicios_java/tema05/Tema05Ejercicio52AlbertoM.java
@@ -0,0 +1,37 @@
+/**
+ * Realiza un programa que sea capaz de desplazar todos los dígitos de un número
+ * de derecha a izquierda una posición. El dígito de más a la izquierda, pasaría a
+ * dar la vuelta y se colocaría a la derecha. Si el número tiene un solo dígito, se
+ * queda igual.
+ *
+ * @author Alberto Moreno González
+ */
+public class Tema05Ejercicio52AlbertoM {
+  public static void main(String[] args) {
+    System.out.println("PROGRAMA CAPAZ DE DESPLAZAR TODOS LOS DIGITOS DE UN NUMERO  DE DERECHA A IZQUIERDA UNA POSICIÓN");
+    System.out.println("------------------------------------------------------------------------------------------------");
+    System.out.println(" ");
+    
+    System.out.println("Introduzca un número, por favor: ");
+    System.out.print("> ");
+    long numero = Long.parseLong(System.console().readLine());
+    System.out.println(" ");
+    
+    long restaurarNumero = numero;
+    int contadorCifras = 0;
+
+    while (numero > 0) {
+      numero /= 10;
+      contadorCifras++;
+    }
+
+    if (restaurarNumero < 10) {
+      System.out.printf("%-20s%d" ,"El número resultante es " ,restaurarNumero);
+    } else {
+      long CifraALaDerecha = (restaurarNumero % ((int)Math.pow(10 ,(contadorCifras - 1))));
+      long CifrasALaIzquierda = (restaurarNumero / ((int)Math.pow(10 ,(contadorCifras - 1))));
+
+      System.out.printf("%-20s%d%d" ,"El número resultante es " ,CifraALaDerecha ,CifrasALaIzquierda);
+    }
+  }
+}
