ecbd265a224913c7c37c59917864b2711559a1de
Alberto <albertomorenogonzalez03@gmail.com>
2022-01-17 18:39:16 +0100

    Añadidas aclaraciones a biblioteca de funciones 1

--- ejercicios_java/tema08/funciones/funcionesTema801.java
+++ ejercicios_java/tema08/funciones/funcionesTema801.java
@@ -23,16 +23,18 @@ public class funcionesTema801 {
    * @author Alberto Moreno Gonzalez
    */
   public static int voltea(long numero) {
-    int darLaVuelta = 0; //en esta variable 
+    int darLaVuelta = 0; //en esta variable se irá guardando el número al que poco a poco se le irá dando la vuelta
     
     while (numero > 0) {
-      darLaVuelta = ((darLaVuelta * 10) + ((int)numero % 10));
+      //el resto del número introducido entre 10 (su último dígito) se irá sumando a lo que haya en darLaVuelta multiplicado por 10 para actualizar darLaVuelta
+      darLaVuelta = ((darLaVuelta * 10) + ((int)numero % 10)); 
       
-      numero /= 10;
+      numero /= 10; //cada vez que el bucle de una vuelta el número introducido se dividirá entre 10, así hasta que este número sea igual a 0.
     } //while (numero > 0)
 
-    return darLaVuelta;
+    return darLaVuelta; //si el número introducido es igual a cero significa que el número ya ha sido volteado, por lo que se devolverá ael resultado
   } //public static int voltea(int numero)
+
 
   /**
    * La función comprueba si el número es o no capicúa
@@ -43,7 +45,8 @@ public class funcionesTema801 {
    * @author Alberto Moreno Gonzalez
    */
   public static boolean esCapicua(long numero) {
-    if (funciones.funcionesTema801.voltea(numero) == numero) {
+    //usando la función voltea, solo necesitamos saber si el resultado final de la función es igual al número introducido, si es así, la función será verdadera; y si no, falsa
+    if (funciones.funcionesTema801.voltea(numero) == numero) { 
       return true;
     } else {
       return false;
@@ -59,12 +62,16 @@ public class funcionesTema801 {
    * @author Alberto Moreno Gonzalez
    */
   public static boolean esPrimo(int numero) {
-    int entreNumero= 0;
+    int entreNumero= 0; //inicializamos la variable entreNumero
 
+    //un número es primo si solo es divisble entre 1 y sí mismo. Todos los números son divisibles entre uno y sí mismo, por lo que para comprobar si un número es primo o no
+    //empezaremos a dividirlo entre el 2 hasta llegar a un número menos que sí mismo. Si el reso de alguna de estas divisiones es 0, significa que el número no es primo
     for (entreNumero = 2; ((entreNumero < numero) && ((numero % entreNumero) != 0)); entreNumero++) { }
 
+    //si el bucle ha recorrido todos los números desde el 2 hasta llegar al número introducido significa que ahora entreNumero es igual al número, lo que significa que ha tenido ninguna
+    //división exacta, por lo que es primo. Ya que no es posible que el bucle funcione con el 1 o el 2, añadimos a ambos en la condición de que sean primos.
     if ((entreNumero == numero) || (numero == 1) || (numero == 2)) {
-      return true;
+      return true; //si el número es primo la variable booleana será verdadera, si no, falsa
     } else {
       return false;
     }
@@ -80,6 +87,7 @@ public class funcionesTema801 {
    * @author Alberto Moreno Gonzalez
    */
   public static int siguientePrimo(int numero) {
+    //para esta función utilizaremos la variable es primo e iremos incrementando en 1 el número introducido hasta ver cual de los siguientes es primo
     do {
       numero++;
     } while(!funciones.funcionesTema801.esPrimo(numero));
@@ -91,14 +99,15 @@ public class funcionesTema801 {
    * La función, dada una base y un exponente devuelve la potencia.
    *
    * @param base base de la potencia
-   * @param exponnte exponente de la potencia
+   * @param exponente exponente de la potencia
    * @return potencia
    * 
    * @author Alberto Moreno Gonzalez
    */
   public static int potencia(int base, int exponente) {
-    int resultado = 1;
+    int resultado = 1; //definiremos el resultado inicialmente como 1 ya que si el número está elevado a 0 su resultado es 1
     
+    //el exponente indica las veces que se repite el bucle. El resultado será igual a la base por el reusltado previo (este cambia en cada iteración)
     for (int numVecesMultiplica = 0; numVecesMultiplica < exponente; numVecesMultiplica++) {
       resultado = (base * resultado);
     }
