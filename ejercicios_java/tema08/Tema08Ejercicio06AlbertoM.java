fc2cc74eafbbc82f44d43a7c70104c0d99e984b6
Alberto <albertomorenogonzalez03@gmail.com>
2022-01-18 09:10:42 +0100

    Añadido ejercicio 6 tema 8 (nuevo)

--- ejercicios_java/tema08/Tema08Ejercicio06AlbertoM.java
+++ ejercicios_java/tema08/Tema08Ejercicio06AlbertoM.java
@@ -2,8 +2,8 @@ import funciones.funcionesTema801;
 import java.util.Scanner;
 
 /**
- * Devuelve el dígito que está en la posición n de un número entero.
- * Se empieza contando por el 0 y de izquierda a derecha.
+ * Realiza un programa que pida un número por teclado y que luego muestre ese
+ * número al revés.
  *
  * @author Alberto Moreno Gonzalez
  */
@@ -11,8 +11,8 @@ public class Tema08Ejercicio06AlbertoM {
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
 
-    System.out.println("PROGRAMA QUE DEVUELVE EL DÍGITO DE LA POSICIÓN INTRODUCIDA DE UN NÚMERO INTRODUCIDO");
-    System.out.println("------------------------------------------------------------------------------------");
+    System.out.println("PROGRAMA QUE PIDE UN NUMERO Y LUEGO LO MUESTRA AL REVÉS");
+    System.out.println("--------------------------------------------------------");
     System.out.println(" ");
     
     System.out.println("Introduzca un número entero, por favor: ");
@@ -20,11 +20,6 @@ public class Tema08Ejercicio06AlbertoM {
     long numero = Long.parseLong(s.nextLine());
     System.out.println(" ");
 
-    System.out.println("Introduzca la posición de la que quiere ver el dígito (la primera posición es la número 0 y va de izquierda a derecha): ");
-    System.out.print("> ");
-    int posicion = Integer.parseInt(s.nextLine());
-    System.out.println(" ");
-    
-    System.out.println("El dígito en la posición número " + posicion + " es el " + funciones.funcionesTema801.digitoN(numero, posicion));
+    System.out.println(numero + " volteado es " + funciones.funcionesTema801.voltea(numero));
   }
 }
