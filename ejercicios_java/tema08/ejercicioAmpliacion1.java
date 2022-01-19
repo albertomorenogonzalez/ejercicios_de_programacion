f76cf5303866a8d7b33f1d23994ce927d364b51e
Alberto <albertomorenogonzalez03@gmail.com>
2022-01-19 08:52:40 +0100

    Añadidas aclaciones en ejercicio ampliación 1

--- ejercicios_java/tema08/ejercicioAmpliacion1.java
+++ ejercicios_java/tema08/ejercicioAmpliacion1.java
@@ -22,20 +22,26 @@ public class ejercicioAmpliacion1 {
     int altura = Integer.parseInt(s.nextLine());
     System.out.println(" ");
 
+    //después de pedir la altura por pantalla, se definen la longitud de la primera línea de caracteres, 
+    //el carácter de los extremos de la línea, los espacios vacíos del interior de la pirámide 
+    //(relleno de la línea) y el número de espacios previos al primer carácter de la priera línea que
+    //escalona a pirámide
     int longitud = 1;
     char extremo = '*';
     char relleno = ' ';
     int numEspacios = altura - 1;
 
+    //el bucle pintará líneas hasta que el número de líneas sea igual a la altura
     for (int i = 0; i < altura; i++) {
-      if (i == altura - 1) {
+      if (i == altura - 1) { //en la última línea todos los caracteres serán los del extremo de la línea
         relleno = '*';
       }
 
-      System.out.print(espacios(numEspacios));
-      System.out.println(linea(longitud, extremo, relleno));
-      longitud+=2;
-      numEspacios--;
+      System.out.print(espacios(numEspacios)); //pintamos los espacios previos
+      System.out.println(linea(longitud, extremo, relleno)); //pintamos la línea
+
+      longitud+=2; //una línea será dos caracteres más larga que la anterior
+      numEspacios--; //en cada línea hay un espacio previo menos que en la anterior
     }
   }
 
@@ -52,6 +58,9 @@ public class ejercicioAmpliacion1 {
    * @author Alberto Moreno Gonzalez
    */
   public static String linea(int longitud, char extremo, char relleno) {
+    //longitud es el número de caracteres de la línea
+    //cuando el bucle se encuentre entre la primera iteración o en la última el caracter pintado
+    //será el del extremo, el resto de veces será el del relleno
     for (int i = 1; i <= longitud; i++) {
       if ((i == 1) || (i == longitud)) {
         System.out.print(extremo);
@@ -73,6 +82,7 @@ public class ejercicioAmpliacion1 {
    * @author Alberto Moreno Gonzalez
    */
   public static String espacios(int numEspacios) {
+    //definido un número de espacios, la función pintará una línea de espacios de esa longitud
     for (int i = 1; i <= numEspacios; i++) {
       System.out.print(" ");
     }
