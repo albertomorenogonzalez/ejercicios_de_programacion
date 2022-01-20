56acd0a45f79972309b46548fb66ed9dead8a50a
Alberto <albertomorenogonzalez03@gmail.com>
2022-01-20 10:42:25 +0100

    Añadido ejercicio 14 tema 8

---
+++ ejercicios_java/tema08/Tema08Ejercicio14AlbertoM.java
@@ -0,0 +1,29 @@
+import funciones.funcionesTema801;
+import java.util.Scanner;
+
+/**
+ * Programa que pega dos números para formar uno.
+ *
+ * @author Alberto Moreno Gonzalez
+ */
+public class Tema08Ejercicio14AlbertoM { 
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+
+    System.out.println("PROGRAMA QUE PEGA DOS NÚMEROS PARA FORMAR UNO");
+    System.out.println("----------------------------------------------");
+    System.out.println(" ");
+    
+    System.out.println("Introduzca un número entero, por favor: ");
+    System.out.print("> ");
+    long numero1 = Long.parseLong(s.nextLine());
+    System.out.println(" ");
+
+    System.out.println("Introduzca otro número entero, por favor: ");
+    System.out.print("> ");
+    long numero2 = Integer.parseInt(s.nextLine());
+    System.out.println(" ");
+  
+    System.out.print(funciones.funcionesTema801.pegaPorDelante(numero2, (int)numero1));
+  }
+}
