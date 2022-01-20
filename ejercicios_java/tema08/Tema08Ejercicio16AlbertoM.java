a7d15db5aa636467a8d35dc007b91405a9660327
Alberto <albertomorenogonzalez03@gmail.com>
2022-01-20 10:57:17 +0100

    Añadido ejercicio 16 tema 8

---
+++ ejercicios_java/tema08/Tema08Ejercicio16AlbertoM.java
@@ -0,0 +1,25 @@
+import funciones.funcionesTema801;
+import java.util.Scanner;
+
+/**
+ * Muestra los números capicúa que hay entre 1 y 99999.
+ *
+ * @author Alberto Moreno Gonzalez
+ */
+public class Tema08Ejercicio16AlbertoM { 
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+
+    System.out.println("PROGRAMA QUE MUESTRA LOS NÚMEROS CAPICÚA QUE HAY ENTRE EL 1 Y EL 99999");
+    System.out.println("-----------------------------------------------------------------------");
+    System.out.println(" ");
+
+    System.out.println("Los números capicúa que hay entre el 1 y el 99999 son:\n");
+
+    for (int i = 1;i <= 99999; i++) {
+      if (funciones.funcionesTema801.esCapicua(i)) {
+        System.out.print(i + " ");
+      }
+    }
+  }
+}
