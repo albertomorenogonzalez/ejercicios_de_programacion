612da2a220f211cbc48936f2f356c9fd8f7f32ec
Alberto <albertomorenogonzalez03@gmail.com>
2022-01-24 20:26:56 +0100

    Añadido ejericicio 23 tema 8

---
+++ ejercicios_java/tema08/Tema08Ejercicio23AlbertoM.java
@@ -0,0 +1,46 @@
+import funciones.funcionesTema802; //importamos la biblioteca de funciones funcionesTema801
+import java.util.Scanner; //importamos la clase Scanner
+
+/**
+ * Programa que devuelve la media del array que se pasa como parámetro.
+ *
+ * @author Alberto Moreno Gonzalez
+ */
+public class Tema08Ejercicio23AlbertoM { 
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in); //declaramos la variable para usar la clase Scanner
+
+    System.out.println("PROGRAMA QUE DEVUELVE LA MEDIA DEL ARRAY QUE SE PASA CÓMO PARÁMETRO");
+    System.out.println("----------------------------------------------------------------------");
+    System.out.println(" ");
+
+    //se piden el tamaño del array, el valor máximo y el valor mínimo del intervalo
+    System.out.println("Introduzca el tamaño del array: ");
+    System.out.print("> ");
+    int tamañoArray = Integer.parseInt(s.nextLine());
+    System.out.println(" ");
+
+    System.out.println("Introduzca el menor número del intervalo de números que quiere: ");
+    System.out.print("> ");
+    int minimo = Integer.parseInt(s.nextLine());
+    System.out.println(" ");
+
+    System.out.println("Introduzca el mayor número del intervalo de números que quiere: ");
+    System.out.print("> ");
+    int maximo = Integer.parseInt(s.nextLine());
+    System.out.println(" ");
+
+    //se define el array
+    int[] array = funciones.funcionesTema802.generaArrayInt(tamañoArray, maximo, minimo); 
+
+    //se muestra el array
+    System.out.println("Array generado - Array de " + tamañoArray + " números cuyo menor número posible puede ser el " + minimo + " y el máximo el " + maximo + ":\n");
+
+    funciones.funcionesTema802.muestraArray(array, tamañoArray); 
+
+    System.out.println("\n ");
+
+    //se calcula y se muestra la media del array
+    System.out.println("La media del array es " + funciones.funcionesTema802.mediaArrayInt(array));
+  }
+}
