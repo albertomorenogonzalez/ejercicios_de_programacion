34ced47614cc41901f0196a9b5ffd3696d8028c9
Alberto <albertomorenogonzalez03@gmail.com>
2022-01-24 12:42:06 +0100

    Corregida la conversión de binario a decimal

--- ejercicios_java/tema08/Tema08Ejercicio17AlbertoM.java
+++ ejercicios_java/tema08/Tema08Ejercicio17AlbertoM.java
@@ -22,21 +22,20 @@ public class Tema08Ejercicio17AlbertoM {
 
     System.out.print(numeroBi + " en binario es igual a ");
 
-    int numeroDe = 0; //inicializamos la variable numeroDe donde guardaremos el resultado
-    int posicion = 0; //declaramos la variable posicion para usar con la función digitoN
+    long numeroDe = 0; //inicializamos la variable numeroDe donde guardaremos el resultado
+    int posicion = 0; //variable que utilizaremos para indicar la psición del dígito binario (empezando por la derecha) y para usarla de exponente
 
-    //creamos un bucle for que no termina hasta que la variable i (que inicializamos en un número menor al número de dígitos del número binario),
-    //llegue a 0. Esta variable i la hemos declarado así ya que la usaremos para sumar a numeroDe 2 elevado a i cuando un dígito sea igual a 1.
-    //que el dígito sea igual a 1 lo iremos comprobando con la función digitoN. Cuando i es igual a digitos(numeroBi) - 1, la variable posicion 
-    //es igual a 0. La variable posición aumenta en 1 en cada iteración. La función dígito N comprueba el dígito de una posición indica. Estas posiciones
-    //se cuentan de izquierda a derecha empezando por la posición 0.
-    for (int i = (funciones.funcionesTema801.digitos(numeroBi) - 1) ; i >= 0; i--) {
-      if ((funciones.funcionesTema801.digitoN(numeroBi, posicion)) == 1) {
-        numeroDe = numeroDe + funciones.funcionesTema801.potencia(2, i);
-      }
+    //creamos un bucle while en el que numeroBi será dividido entre 10 en cada iteración hasta llegar a 0. El resto de cada división corresponde a un dígito
+    //binario, empezando por la derecha. La variable posicion indica la posición de ese número de derecha a izquierda (aumenta en 1 en cada iteración). 
+    //Una vez obtenido el dígito se multiplica por dos elevado a posicion y se le suma a lo que ya hay en numeroDe. Si digitoBi es 0 no se le sumará nada
+    //a numeroDe, por contra, si es 1, se le sumará la potencia de 2 correspondiente a la posición.
+    while (numeroBi > 0) {
+      int digitoBi = (int)numeroBi % 10;
+      numeroDe = numeroDe + funciones.funcionesTema801.potencia(2, posicion) * digitoBi;
 
       posicion++;
-    }
+      numeroBi /= 10;
+    } //while (numeroBi > 0)
 
     System.out.println(numeroDe + " en decimal"); //mostramos los resultados
   }
