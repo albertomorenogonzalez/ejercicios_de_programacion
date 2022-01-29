3ee8a3a8b3ca39d0dfd76d09b4630dafcb70f802
Alberto <albertomorenogonzalez03@gmail.com>
2022-01-29 21:17:39 +0100

    Añadida función generaArrayBiInt y terminada función muestraArrayBiEnTabla en funcionesTema803

---
+++ ejercicios_java/tema08/Tema08Ejercicio29AlbertoM.java
@@ -0,0 +1,47 @@
+import funciones.funcionesTema803; //importamos la biblioteca de funciones funcionesTema803
+import java.util.Scanner; //importamos la clase Scanner
+
+/**
+ * Programa que enera un array de tamaño n x m con números
+ * aleatorios cuyo intervalo (mínimo y máximo) se indica como parámetro.
+ *
+ * @author Alberto Moreno Gonzalez
+ */
+public class Tema08Ejercicio29AlbertoM { 
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in); //declaramos la variable para usar la clase Scanner
+
+    System.out.println("PROGRAMA QUE GENERA UN ARRAY DE TAMAÑO N X M CON NÚMEROS ALEATORIOS CUYO INTERVALO SE PIDE POR PANTALLA");
+    System.out.println("--------------------------------------------------------------------------------------------------------");
+    System.out.println(" ");
+
+    //se piden las filas y las columnas del array, el valor máximo y el valor mínimo del intervalo
+    System.out.println("Introduzca las filas del array: ");
+    System.out.print("> ");
+    int filasArray = Integer.parseInt(s.nextLine());
+    System.out.println(" ");
+
+    System.out.println("Introduzca las columnas del array: ");
+    System.out.print("> ");
+    int columnasArray = Integer.parseInt(s.nextLine());
+    System.out.println(" ");
+
+    System.out.println("Introduzca el menor número del intervalo de números que quiere: ");
+    System.out.print("> ");
+    int minimo = Integer.parseInt(s.nextLine());
+    System.out.println(" ");
+
+    System.out.println("Introduzca el mayor número del intervalo de números que quiere: ");
+    System.out.print("> ");
+    int maximo = Integer.parseInt(s.nextLine());
+    System.out.println(" ");
+
+    //se define el array
+    int[][] array = funciones.funcionesTema803.generaArrayBiInt(filasArray, columnasArray, maximo, minimo); 
+
+    //se muestra el array
+    System.out.println("Array generado - Array de " + filasArray + " filas  y " + columnasArray + " columnas cuyo menor número posible puede ser el " + minimo + " y el máximo el " + maximo + ":\n");
+
+    funciones.funcionesTema803.muestraArrayBiEnTabla(array, filasArray, columnasArray); 
+  }
+}
--- ejercicios_java/tema08/funciones/funcionesTema803.java
+++ ejercicios_java/tema08/funciones/funcionesTema803.java
@@ -36,14 +36,106 @@ public class funcionesTema803 {
    * (Los números tendrán que ser como máximo de tres cifras)
    *
    * @param array array que se quiere mostrar
-   * @param filas números de filas del array
-   * @param columnas número de columnas del array
-   * @return los valores de un array
+   * @param filas filas del array
+   * @param columnas columnas del array
+   * @return los valores de un array bidimensional en una tabla
    * 
    * @author Alberto Moreno Gonzalez
    */
   public static void muestraArrayBiEnTabla(int[][] array, int filas, int columnas) {
-    
-    
-  } //public static void muestraArrayBiEnTabla(int[] array, int tamaño)
+    int i; //declaramos la variable que utilizaremos como índice principal
+
+    //se muestra el número de columna
+    for (i = 0; i < columnas; i++) {
+      System.out.print("     " + i);
+    }
+
+    System.out.println(" "); //salto de línea
+
+    //se muestra el borde superior de la tabla
+    for (i = 0; i < columnas; i++) {
+      if (i == 0) {
+        System.out.print("  ┌─────┬");
+      } else if (i == columnas - 1) {
+        System.out.print("─────┐");
+      } else {
+        System.out.print("─────┬");
+      } //if (i == 0)
+    } //for (int i = 0; i < tamaño; i++)
+
+    System.out.println(" ");
+
+    //se muestra el resto de la tabla
+    for (i = 0; i < filas; i++) {
+      System.out.print(i + " "); //se muestra el número de fila
+
+      //se muestra el borde izquierdo y el valor de cada celda
+      for (int j = 0; j < columnas; j++) {
+        System.out.printf("%-1s %3d " ,"|" ,array[i][j]);
+      }
+
+      System.out.print("|"); //se meustra el borde derecho de cada celda
+
+      System.out.println(" ");
+
+      int k; //declaramos la variable k para usarla de índice al mostrar cada borde inferior
+
+      //si i es igual filas - 1 se mostrará el borde inferior de la tabla
+      //en el resto de caso se mostrará el borde inferior de la fila y un salto de línea
+      if (i == filas- 1) {
+        for (k = 0; k < columnas; k++) {
+          if (k == 0) {
+            System.out.print("  └─────┴");
+          } else if (k == columnas - 1) {
+            System.out.print("─────┘");
+          } else {
+            System.out.print("─────┴");
+          } //if (k == 0)
+        } //for (k = 0; k < columnas; k++)
+      } else {
+        for (k = 0; k < columnas; k++) {
+          if (k == 0) {
+            System.out.print("  ├─────┼");
+          } else if (k == columnas - 1) {
+            System.out.print("─────┤");
+          } else {
+            System.out.print("─────┼");
+          } //if (k == 0)
+        } //for (k = 0; k < columnas; k++)
+
+        System.out.println(" ");
+      } //if (filas == columnas - 1)
+    } //for (i = 0; i < filas; i++)
+  } //public static void muestraArrayEnTabla(int[][] array, int filas, int columnas)
+
+
+  /**
+   * La función genera un array de tamaño n x m con números
+   * aleatorios cuyo intervalo (mínimo y máximo) se indica como parámetro.
+   *
+   * @param array array que se quiere mostrar
+   * @param maximo límite mayor del intervalo
+   * @param minimo límite menor del intervalo
+   * @return array de tamaño n de números aleatorios entre minimo y maximo
+   * 
+   * @author Alberto Moreno Gonzalez
+   */
+  public static int[][] generaArrayBiInt(int filas, int columnas, int maximo, int minimo) {
+    //se define el array que se va a generar
+    int[][] array = new int[filas][columnas];
+
+    //con el bucle se asigna un valor en cada posción del array bidimensional.
+    //Se van asignando por filas gracias a la variable i y dentro de cada fila se asiga por columna
+    //gracias a la variable j. El intervalo de los valores aleatorios se definirá
+    //multiplicando la función Math.random() por el mayor número del intervalo menos el mínimo,
+    //para que al sumarle el mínimo el valor máximo no cambie.
+    for (int i = 0; i < filas; i++) {
+      for (int j = 0; j < columnas; j++) {
+        array[i][j] = ((int)(Math.random() * (maximo - minimo)) + minimo);
+      }
+
+    } //for (int i = 0; i < filas; i++)
+
+    return array;
+  } //public static void generaArrayBiInt(int[][] array, int filas, int columnas)
 }
